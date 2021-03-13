04b2a8f52cc0a37d1c628745f135236cb770bb0f
Alex Karcher <alkarche@microsoft.com>
2021-03-13 11:26:41 -0800

    Added object mapping

--- API/TreeData/TreeDataMock.cs
+++ API/TreeData/TreeDataMock.cs
@@ -24,6 +24,22 @@ namespace TreeData_CLI
         public string Codes { get; set; }
         public string Comments { get; set; }
     }
+    public class TreeStorage {
+        public string TempID { get; set; }
+        public string QuadratName { get; set; }
+        public string Tag { get; set; }
+        public string StemTag { get; set; }
+        public string Mnemonic_as_SpCode { get; set; }
+        public string DBH { get; set; }
+        public string Codes { get; set; }
+        public string HOM { get; set; }
+        public string ExactDate { get; set; }
+        public string x { get; set; }
+        public string y { get; set; }
+        public string PlotID { get; set; }
+        public string CensusID { get; set; }
+        public string Errors { get; set; }
+    }
     public class TreeResponse
     {
         public int Tag { get; set; }
@@ -63,7 +79,7 @@ namespace TreeData_CLI
 
         [FunctionName("MySQLTest")]
         public static async Task<IActionResult> MySQLTest(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "MySQLTest")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "TreeData")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -71,20 +87,46 @@ namespace TreeData_CLI
             string connStr = System.Environment.GetEnvironmentVariable("MySQLConnection", EnvironmentVariableTarget.Process);
             
             MySqlConnection conn = new MySqlConnection(connStr);
+            var LoadingResponse = new ArrayList();
             try
             {
                 log.LogInformation("Connecting to MySQL...");
                 conn.Open();
-                // Perform database operations
+
+                string sql = "select TempID, QuadratName, Tag, StemTag, Mnemonic as SpCode, DBH, Codes, HOM, ExactDate, x, y, PlotID, CensusID, Errors from tempnewplants";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                
+                while (rdr.Read())
+                {
+                    LoadingResponse.Add(new TreeStorage{
+                        TempID = rdr["TempID"].ToString(),
+                        QuadratName = rdr["QuadratName"].ToString(),
+                        Tag = rdr["Tag"].ToString(),
+                        StemTag = rdr["StemTag"].ToString(),
+                        Mnemonic_as_SpCode = rdr[4].ToString(),
+                        DBH = rdr["DBH"].ToString(),
+                        Codes = rdr["Codes"].ToString(),
+                        HOM = rdr["HOM"].ToString(),
+                        ExactDate = rdr["ExactDate"].ToString(),
+                        x = rdr["x"].ToString(),
+                        y = rdr["y"].ToString(),
+                        PlotID = rdr["PlotID"].ToString(),
+                        CensusID = rdr["CensusID"].ToString(),
+                        Errors = rdr["Errors"].ToString()
+                    });
+                }
+                rdr.Close();
             }
             catch (Exception ex)
             {
                 log.LogInformation(ex.ToString());
+                //return new FailedObjectResult("Failed to process request");
             }
             conn.Close();
             log.LogInformation("Done.");
 
-            return new OkObjectResult("Test");
+            return new OkObjectResult(JsonConvert.SerializeObject(LoadingResponse));
         }
     }
 }
