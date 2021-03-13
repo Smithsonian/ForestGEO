4c131fc205e2df8e14607cf1f90503725d555e88
Alex Karcher <alkarche@microsoft.com>
2021-03-13 13:49:15 -0800

    Checking Trees against real data

--- API/TreeData/TreeDataMock.cs
+++ API/TreeData/TreeDataMock.cs
@@ -23,13 +23,22 @@ namespace TreeData_CLI
         public int DBH { get; set; }
         public string Codes { get; set; }
         public string Comments { get; set; }
+        public bool WasDead(ArrayList Trees){
+            foreach(TreeStorage tree in Trees){
+                if(tree.Tag == Tag && tree.StemTag == StemTag
+                    && tree.Codes == "dt" ){
+                    return true;
+                }
+            }
+            return false;
+        }
     }
     public class TreeStorage {
         public string TempID { get; set; }
         public string QuadratName { get; set; }
-        public string Tag { get; set; }
-        public string StemTag { get; set; }
-        public string Mnemonic_as_SpCode { get; set; }
+        public int Tag { get; set; }
+        public int StemTag { get; set; }
+        public string SpCode { get; set; }
         public string DBH { get; set; }
         public string Codes { get; set; }
         public string HOM { get; set; }
@@ -43,9 +52,9 @@ namespace TreeData_CLI
         public TreeStorage MapSQL(MySqlDataReader rdr){
             TempID = rdr["TempID"].ToString();
             QuadratName = rdr["QuadratName"].ToString();
-            Tag = rdr["Tag"].ToString();
-            StemTag = rdr["StemTag"].ToString();
-            Mnemonic_as_SpCode = rdr[4].ToString();
+            Tag = rdr.GetInt32("Tag");
+            StemTag = rdr.GetInt32("StemTag");
+            SpCode = rdr["SpCode"].ToString();
             DBH = rdr["DBH"].ToString();
             Codes = rdr["Codes"].ToString();
             HOM = rdr["HOM"].ToString();
@@ -64,54 +73,25 @@ namespace TreeData_CLI
         public int StemTag { get; set; }
         public int ErrorCode { get; set; }
         public string Error { get; set; }
+        public TreeResponse (TreeRequest tree, int ecode, string error){
+            Tag = tree.Tag;
+            StemTag = tree.StemTag;
+            ErrorCode = ecode;
+            Error = error;
+        }
     }
     public static class TreeDataMock
     {
-        [FunctionName("TreeDataMock")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "TreeData")] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+        private static string connStr = System.Environment.GetEnvironmentVariable("MySQLConnection", EnvironmentVariableTarget.Process);
+        private static MySqlConnection conn = new MySqlConnection(connStr);
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var data = JsonConvert.DeserializeObject<TreeRequest[]>(requestBody);
-
-            string responseMessage = data[0].SpCode;
-
-            //Create fake errors
-            var TreeResponse = new ArrayList();
-            foreach(var Tree in data){
-                if(Tree.Codes == "dt"){
-                    TreeResponse.Add(new TreeResponse{
-                        Tag = Tree.Tag, 
-                        StemTag = Tree.StemTag,
-                        ErrorCode = 1,
-                        Error = "Bad Tree"
-                    });
-                }
-            }
-
-            return new OkObjectResult(JsonConvert.SerializeObject(TreeResponse));
-        }
-
-        [FunctionName("GetTrees")]
-        public static IActionResult GetTrees(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "TreeData")] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string connStr = System.Environment.GetEnvironmentVariable("MySQLConnection", EnvironmentVariableTarget.Process);
-            
-            MySqlConnection conn = new MySqlConnection(connStr);
+        private static ArrayList QueryTreeDB(string sql){
             var LoadingResponse = new ArrayList();
             try
             {
-                log.LogInformation("Connecting to MySQL...");
+                Console.WriteLine("Connecting to MySQL...");
                 conn.Open();
 
-                string sql = "select TempID, QuadratName, Tag, StemTag, Mnemonic as SpCode, DBH, Codes, HOM, ExactDate, x, y, PlotID, CensusID, Errors from tempnewplants";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 MySqlDataReader rdr = cmd.ExecuteReader();
                 
@@ -121,13 +101,47 @@ namespace TreeData_CLI
             }
             catch (Exception ex)
             {
-                log.LogInformation(ex.ToString());
+                Console.WriteLine(ex.ToString());
                 //return new FailedObjectResult("Failed to process request");
             }
             conn.Close();
-            log.LogInformation("Done.");
+            Console.WriteLine("Done.");
+            
+            return LoadingResponse;
+        }
+
+        [FunctionName("GetTrees")]
+        public static IActionResult GetTrees(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "TreeData")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            ArrayList LoadingResponse = QueryTreeDB("select TempID, QuadratName, Tag, StemTag, Mnemonic as SpCode, DBH, Codes, HOM, ExactDate, x, y, PlotID, CensusID, Errors from tempnewplants");
 
             return new OkObjectResult(JsonConvert.SerializeObject(LoadingResponse));
+        }
+
+        [FunctionName("PostTrees")] 
+        public static async Task<IActionResult> PostTrees(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "TreeData")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<TreeRequest[]>(requestBody);
+
+            ArrayList LoadingResponse = QueryTreeDB("select TempID, QuadratName, Tag, StemTag, Mnemonic as SpCode, DBH, Codes, HOM, ExactDate, x, y, PlotID, CensusID, Errors from tempnewplants");
+
+            var TreeResponse = new ArrayList();
+            foreach(var Tree in data){
+                if(Tree.Codes =="at" && Tree.WasDead(LoadingResponse)){
+                    TreeResponse.Add(new TreeResponse(Tree, 1, "Bad Tree"));
+                }
+            }
+
+            return new OkObjectResult(JsonConvert.SerializeObject(TreeResponse));
         }
     }
 }
