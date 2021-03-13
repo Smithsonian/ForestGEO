cd0ac85292cc1e52e0604688b770c918308c0748
Cesar Zapata <cezapata@microsoft.com>
2021-03-13 12:35:55 -0800

    Fleshes out initial UI for quadrat data entry

--- webapp/src/main/new/New.tsx
+++ webapp/src/main/new/New.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+
 import { QuadratDataEntryForm } from "./QuadratDataEntryForm";
 import { QuadratMetadataEntryForm } from "./QuadratMetadataEntryForm";
 
-export const New = () => (
-    <>
-        <h1>New Plant Form</h1>
-        <QuadratMetadataEntryForm />
-        <QuadratDataEntryForm />
-    </>
-);
+export const New = () => {
+    const [errors, setErrors] = useState([]);
+    const data: any[] = [];
+    return (
+        <>
+            <h1>New Plant Form</h1>
+            <QuadratMetadataEntryForm />
+            <QuadratDataEntryForm />
+            <button onClick={() => {
+                // handle form submission
+                fetch('', { method: 'POST', body: JSON.stringify(data)})
+                    .then(() => {
+                        setErrors([])
+                    })
+                    .catch(error => {
+                        setErrors(error);
+                    })
+            }}>Submit</button>
+            <pre>
+                {JSON.stringify(errors, null, 2)}
+            </pre>
+        </>
+    );
+}
 
 New.defaultName = 'New';
--- webapp/src/main/new/QuadratDataEntryForm/QuadratDataEntryForm.tsx
+++ webapp/src/main/new/QuadratDataEntryForm/QuadratDataEntryForm.tsx
@@ -1,2 +1,49 @@
-export const QuadratDataEntryForm = () => <div>QuadratDataEntryForm</div>;
+import { useState } from 'react';
+
+const Stem = () => (
+    <tr>
+        <td><input type="text"/></td>
+        <td><input type="text"/></td>
+        <td><input type="text"/></td>
+        <td><input type="text"/></td>
+        <td><input type="text"/></td>
+        <td><input type="text"/></td>
+        <td><input type="text"/></td>
+        <td><button>Remove</button></td>
+    </tr>
+);
+
+export const QuadratDataEntryForm = () => {
+    const [stems, setStems] = useState<JSX.Element[]>([]);
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Subquadrat</th>
+                        <th>Tag</th>
+                        <th>StemTag</th>
+                        <th>SpCode</th>
+                        <th>DBH</th>
+                        <th>Codes</th>
+                        <th>Comments</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {stems}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td>
+                            <button onClick={() => {
+                                setStems([...stems, Stem()]);
+                            }}>Add stem</button>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </>
+    );
+};
 QuadratDataEntryForm.defaultName = 'QuadratDataEntryForm';
