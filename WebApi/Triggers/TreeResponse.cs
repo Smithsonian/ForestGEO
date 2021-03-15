2163e8c5ec5317627539f36f28dd32d2db330399
Alex Karcher <alkarche@microsoft.com>
2021-03-14 17:20:14 -0700

    Moved classes to separate files
    Also renamed a ton of variables and swapped out arraylist of dictionary

---
+++ WebApi/Triggers/TreeResponse.cs
@@ -0,0 +1,19 @@
+namespace ForestGEO.WebApi.Triggers.Tree
+{
+    public class TreeResponse
+    {
+        public string Subquadrat { get; set; }
+        public string Tag { get; set; }
+        public string StemTag { get; set; }
+        public int ErrorCode { get; set; }
+        public string Error { get; set; }
+        public TreeResponse (Tree tree, int ecode, string error)
+        {
+            Subquadrat = tree.Subquadrat;
+            Tag = tree.Tag;
+            StemTag = tree.StemTag;
+            ErrorCode = ecode;
+            Error = error;
+        }
+    }
+}
