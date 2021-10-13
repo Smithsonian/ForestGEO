5e7ed9c64617a329010a429e68f8f0d1585a3bc3
Alex Karcher <alkarche@microsoft.com>
2021-10-13 15:58:06 -0700

    Added cosmos DB connection for GET

---
+++ WebApi/Controllers/CosmosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Cosmos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ForestGEO.WebApi.Triggers.Tree
+{
+    internal class CosmosController
+    {
+        public static async Task<List<Model.Contracts.Tree>> QueryCosmos(string sqlQueryText, Container container)
+        {
+
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+            FeedIterator<Model.Contracts.Tree> queryResultSetIterator = container.GetItemQueryIterator<Model.Contracts.Tree>(queryDefinition);
+
+            List<Model.Contracts.Tree> trees = new List<Model.Contracts.Tree>();
+
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<Model.Contracts.Tree> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (Model.Contracts.Tree tree in currentResultSet)
+                {
+                    trees.Add(tree);
+                }
+            }
+
+            return trees;
+        }
+    }
+}
--- WebApi/Triggers/TreeTriggers.cs
+++ WebApi/Triggers/TreeTriggers.cs
@@ -7,19 +7,35 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.Cosmos;
+using ForestGEO.WebApi.Model.Contracts;
+using System.Collections.Generic;
 
 namespace ForestGEO.WebApi.Triggers.Tree
 {
     public static class TreeTriggers
     {
+        private static readonly string EndpointUri = System.Environment.GetEnvironmentVariable("CosmosEndpointUri", EnvironmentVariableTarget.Process);
+        private static readonly string PrimaryKey = System.Environment.GetEnvironmentVariable("CosmosPrimaryKey", EnvironmentVariableTarget.Process);
+        private static string databaseId = System.Environment.GetEnvironmentVariable("CosmosDatabaseId", EnvironmentVariableTarget.Process);
+        private static string containerId = System.Environment.GetEnvironmentVariable("CosmosContainerId", EnvironmentVariableTarget.Process);
+
+        private static CosmosClient cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+        private static readonly Database database = TreeTriggers.cosmosClient.GetDatabase(TreeTriggers.databaseId);
+        private static readonly Container container = TreeTriggers.database.GetContainer(TreeTriggers.containerId);
+
         [FunctionName("GetCensus")]
-        public static IActionResult GetCensus(
+        public static async Task<IActionResult> GetCensus(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "Census")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            return new OkObjectResult(JsonConvert.SerializeObject(MockData.TreeData));
+            var sqlQueryText = "SELECT * FROM c";
+
+            List<Model.Contracts.Tree> trees = await CosmosController.QueryCosmos(sqlQueryText, TreeTriggers.container);
+
+            return new OkObjectResult(JsonConvert.SerializeObject(trees));
         }
         [FunctionName("PostCensus")]
         public static async Task<IActionResult> PostCensus(
