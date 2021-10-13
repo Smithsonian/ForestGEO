5868e93297614a24bda10821fefbaaf74e7eaa56
shmartel <53022511+shmartel@users.noreply.github.com>
2021-10-13 16:10:57 -0700

    renaming SideId to PlotId, per db schema

--- WebApi/Models/Contracts/Tree.cs
+++ WebApi/Models/Contracts/Tree.cs
@@ -7,7 +7,7 @@ namespace ForestGEO.WebApi.Model.Contracts
         // The Type property is used to differentiate all the different types of documents in the Cosmos DB (ie, "tree" vs "plot", etc)
         public string Type => "tree";
         public int CensusId {  get; set; }
-        public int SiteId {  get; set; }
+        public int PlotId {  get; set; }
         public string Subquadrat { get; set; }
         public int Tag { get; set; }
         public int StemTag { get; set; }
