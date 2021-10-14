8382579197002a953f00f2d91755622477f15207
aklaran <botbunk@gmail.com>
2021-10-14 14:50:22 -0700

    Adds Andrea's pre-validations

--- webapp/src/validation/preValidation.ts
+++ webapp/src/validation/preValidation.ts
@@ -11,5 +11,30 @@ export function preValidate(row: number, columnName: string, value: any): Set<Va
     })
   }
 
+  if (columnName === "DBH"){
+    if (!value) {
+      validationErrors.add({
+        index: row,
+        column: columnName,
+        errorMessage:"Gotta enter a value"
+      })
+    }
+
+    if (parseInt(value, 10) > 200) {
+      validationErrors.add({
+        index: row,
+        column: columnName,
+        errorMessage:"Oops! That value is too high."
+      })
+    }
+  
+    if (parseInt(value, 10) < 1) {
+      validationErrors.add({
+        index: row,
+        column: columnName,
+        errorMessage:"Oops! That value is too low."
+      })    }
+  }
+
   return validationErrors
 }
