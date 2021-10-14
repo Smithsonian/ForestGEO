d56ca31489ced1b9837eb2168bd38dc8f46a2297
Vivien Hung <vivihung@microsoft.com>
2021-10-14 11:57:48 -0700

    Integrate storage with api (#16)
    Integrate storage with api (function app)
    
    App Behavior:
    Get N-1 census data
    - GetLatestCensus response will be stored to local
    - The response of GetLatestCensus will be displayed as the default rows of the table in old plant form
    - If the app is offline, we will get latest census data from local.
    
    Caviar: If a user is inputting data to the form but the network status changes (online <-> offline), it seems we will wipe out the previous user input (which isn't submitted yet). This will have to be addressed later.
    
    Insert user input data
    After user click submit button,
    
    - If the app is online, data will be sent to api (HTTP POST)
    - If the app is offline, data will be stored locally. When the app is online, a web worker will upload the local user data to api and clear the table.
    
    Change Details
    - Manually handle online/offline status for caching CORS requests. Service worker doesn't support handling service call with different domains.
    - Use web worker to upload user input data that is stored locally.
    - Create react context for connectivity status (online or offline) and tables (stores) reference of IndexedDB.
    - The data model is once again updated based on what we get from the API.
    
    UI Changes
    - Prepopulate form with the n-1 census data
    - Add online/offline state in the page
    - Add progressive bar when uploading data to the cloud.
    - Add loading shimmer for form while loading data.
    
    Co-authored-by: Vivien Hung <windfish_hung@hotmail.com>

--- webapp/src/main/new/New.tsx
+++ webapp/src/main/new/New.tsx
@@ -1,48 +1,60 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
+import { Shimmer, ShimmerElementType } from "@fluentui/react/lib/Shimmer";
 
 import { QuadratDataEntryForm } from "./QuadratDataEntryForm";
 import { QuadratMetadataEntryForm } from "./QuadratMetadataEntryForm";
-import { Stem } from "../../types";
-import { insertStems } from "./dataService";
 import { columns } from "./QuadratDataEntryForm/columnHeaders";
-import { mockCensusData } from "../../mockData/mockCensusData";
+
 import {
   postValidate,
   PostValidationError,
 } from "../../validation/postValidation";
+import { getCensus, insertCensus } from "./dataService";
+import { Stem } from "../../types";
+import { useStorageContext } from "../../context/storageContext";
+import { useConnectivityContext } from "../../context/connectivityContext";
+import { getAllItems } from "../../helpers/storageHelper";
 
 export const New = () => {
+  const { latestCensusStore, userInputStore } = useStorageContext();
+  const { isOnline } = useConnectivityContext();
+
   const [errors, setErrors] = useState<any>([]);
-
-  const hardCodedFormData: Stem[] = [
-    {
-      Subquadrat: 11,
-      Tag: 1,
-      SpCode: "species",
-      DBH: 10,
-      Htmeas: 1.5,
-      Codes: "at",
-      Comments: "",
-    },
-    {
-      Subquadrat: 11,
-      Tag: 2,
-      SpCode: "species",
-      DBH: 10,
-      Htmeas: 1.5,
-      Codes: "at",
-      Comments: "",
-    },
-  ];
-
-  const [formData] = useState(hardCodedFormData);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Table data has to be memoized for react-table performance
   const columnHeaders = useMemo(() => columns, []);
-  const [data, setData] = useState(useMemo(() => mockCensusData, []));
+  const [data, setData] = useState<Stem[]>([]);
   const [postValidationErrors, setPostValidationErrors] = useState<
     PostValidationError[]
   >([]);
+
+  useEffect(() => {
+    if (isOnline) {
+      setIsLoading(true);
+      getCensus().then((response) => {
+        setData(response);
+        setIsLoading(false);
+
+        // Refresh storage
+        latestCensusStore
+          ?.clear()
+          .then(() =>
+            response.forEach((census) =>
+              latestCensusStore
+                ?.setItem(census.CensusId.toString(), census)
+                .catch((error) => console.error(error))
+            )
+          )
+          .catch((error) => console.error(error));
+      });
+    } else if (latestCensusStore) {
+      // Get the latest census from local when offline
+      getAllItems<Stem>(latestCensusStore).then((localCensus) =>
+        setData(localCensus)
+      );
+    }
+  }, [isOnline, latestCensusStore, setIsLoading, setData]);
 
   const updateData = (rowIndex: number, columnId: string, value: any) => {
     console.log(`set data for ${columnId} at row ${rowIndex} to ${value}`);
@@ -64,33 +76,61 @@ export const New = () => {
     <>
       <h1>Old Trees Form</h1>
       <QuadratMetadataEntryForm />
-      <QuadratDataEntryForm
-        columns={columnHeaders}
-        data={data}
-        updateHandler={updateData}
-        postValidationErrors={postValidationErrors}
-      />
-      <button
-        type="submit"
-        onClick={() => {
-          const postErrors = postValidate(data);
-          setPostValidationErrors(postErrors);
-          if (postErrors.length === 0) {
-            insertStems(formData).catch((error) => setErrors(error));
-          }
-        }}
-      >
-        Submit
-      </button>
-      <ul>
-        {errors.map((error: any) => (
-          <li>
-            There was an error on the tree branch identified by subquadrat{" "}
-            {error.Subquadrat}, tag {error.Tag} and stem {error.StemTag}:{" "}
-            {error.Error}
-          </li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <Shimmer
+          shimmerElements={[
+            { type: ShimmerElementType.line, height: 16, width: "20%" },
+            { type: ShimmerElementType.gap, width: "2%" },
+            { type: ShimmerElementType.line, height: 16, width: "20%" },
+            { type: ShimmerElementType.gap, width: "2%" },
+            { type: ShimmerElementType.line, height: 16, width: "20%" },
+            { type: ShimmerElementType.gap, width: "2%" },
+            { type: ShimmerElementType.line, height: 16, width: "20%" },
+            { type: ShimmerElementType.gap, width: "2%" },
+            { type: ShimmerElementType.line, height: 16 },
+          ]}
+        />
+      ) : (
+        <>
+          <QuadratDataEntryForm
+            columns={columnHeaders}
+            data={data}
+            updateHandler={updateData}
+            postValidationErrors={postValidationErrors}
+          />
+          <button
+            type="submit"
+            onClick={() => {
+              const postErrors = postValidate(data);
+              setPostValidationErrors(postErrors);
+              if (postErrors.length === 0) {
+                if (isOnline) {
+                  // Submit to cloud when online
+                  insertCensus(data).catch((error) => setErrors(error));
+                } else {
+                  // Save locally when offline
+                  data.forEach((census) =>
+                    userInputStore
+                      ?.setItem(census.CensusId.toString(), census)
+                      .catch((error) => console.error(error))
+                  );
+                }
+              }
+            }}
+          >
+            Submit
+          </button>
+          <ul>
+            {errors.map((error: any) => (
+              <li>
+                There was an error on the tree branch identified by subquadrat{" "}
+                {error.Subquadrat}, tag {error.Tag} and stem {error.StemTag}:{" "}
+                {error.Error}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 };
--- webapp/src/types/index.ts
+++ webapp/src/types/index.ts
@@ -1,24 +1,27 @@
 export type Subquadrat = {
-    id: string;
-    trees: Tree[];
+  id: string;
+  trees: Tree[];
 };
 
 export type Quadrat = {
-    id: string;
-    subquadrats: Subquadrat[];
+  id: string;
+  subquadrats: Subquadrat[];
 };
 
 export type Tree = {
-    tag: number;
-    stems: Stem[];
+  tag: number;
+  stems: Stem[];
 };
 
 export type Stem = {
-    Tag: number;
-    Subquadrat: number;
-    SpCode: string;
-    DBH: number;
-    Htmeas: number;
-    Codes: string;
-    Comments: string;
-}
+  Type: string;
+  CensusId: number;
+  SiteId: 0;
+  Tag: number;
+  Subquadrat: number;
+  SpCode: string;
+  StemTag:number;
+  DBH: number;
+  Codes: string;
+  Comments: string;
+};
