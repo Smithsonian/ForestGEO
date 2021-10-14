a3a47e5699b0a366cec503e5988b11982bd883f6
Vivien Hung <vivihung@microsoft.com>
2021-10-14 09:48:40 -0700

    Make the webapp PWA (#15)
    Make the webapp PWA.
    
    1. Add required npm packages.
    2. Add service worker files.
    3. Update icons.
    
    Note:
    This change includes online/offline detection and IndexedDB test code.
    We haven't enable cache for cross origin requests (e.g., the request to function app). It will be in another PR.
    
    Co-authored-by: Vivien Hung <windfish_hung@hotmail.com>

--- webapp/src/main/Main.tsx
+++ webapp/src/main/Main.tsx
@@ -1,19 +1,71 @@
 import { Switch, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import localforage from "localforage";
 
 import { Home } from "./home";
 import { New } from "./new";
 
-export const Main = () => (
-  <main>
-    <Switch>
-      <Route strict={false} exact path="/">
-        <Home />
-      </Route>
-      <Route path="/new">
-        <New />
-      </Route>
-    </Switch>
-  </main>
-);
+export const Main = () => {
+  const [online, setOnline] = useState(navigator.onLine);
+
+  function handleOnline() {
+    setOnline(true);
+  }
+
+  function handleOffline() {
+    setOnline(false);
+  }
+
+  useEffect(() => {
+    // Test IndexedDB
+    const userInputStore = localforage.createInstance({
+      name: "ForestGEO App Storage",
+      storeName: "user-input-store",
+      description:
+        "The local storage for user input records that are validated locally.",
+    });
+
+    const latestCensusStore = localforage.createInstance({
+      name: "ForestGEO App Storage",
+      storeName: "latest-census-store",
+      description:
+        "The local storage for latest census data cache from the cloud. Use only for validation.",
+    });
+
+    userInputStore.setItem("1", {
+      Subquadrat: "11",
+      Tag: 1,
+      SpCode: "species",
+      DBH: 10,
+      Htmeas: 1.5,
+      Codes: "at",
+      Comments: "",
+    });
+
+    // Register event listeners for network status change.
+    // Note the initial network status when app load comes from navigator.onLine.
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, [setOnline]);
+
+  return (
+    <main>
+      <span>{online ? "Online" : "Offline"}</span>
+      <Switch>
+        <Route strict={false} exact path="/">
+          <Home />
+        </Route>
+        <Route path="/new">
+          <New />
+        </Route>
+      </Switch>
+    </main>
+  );
+};
 
 Main.defaultName = "Main";
