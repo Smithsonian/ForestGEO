459aea9f31714190e538ee3261e431cf28d0cd66
aklaran <botbunk@gmail.com>
2021-10-14 14:39:40 -0700

    null check and removing eslint barriers

--- webapp/src/validation/postValidation.ts
+++ webapp/src/validation/postValidation.ts
@@ -5,12 +5,14 @@ export function postValidate(data: Tree[]): Set<ValidationError> {
   const errors = new Set<ValidationError>()
 
   data.forEach(stem => {
-    if (stem.DBH.toString() === "9001") {
-      errors.add({
-        index: data.indexOf(stem),
-        column: "DBH",
-        errorMessage: "That's a pretty wide tree"
-      })
+    if (stem && stem.DBH) {
+      if (stem.DBH.toString() === "9001") {
+        errors.add({
+          index: data.indexOf(stem),
+          column: "DBH",
+          errorMessage: "That's a pretty wide tree"
+        })
+      }
     }
   });
 
