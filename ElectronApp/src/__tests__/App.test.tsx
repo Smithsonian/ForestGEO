e91981563c54a6c83b46c81603f8480748e7ff3f
esscott <esscott@smith.edu>
2022-01-17 17:04:41 -0500

    Unit test for full app rendering/navigating

--- ElectronApp/src/__tests__/App.test.tsx
+++ ElectronApp/src/__tests__/App.test.tsx
@@ -1,9 +1,20 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../renderer/App';
 
-describe('App', () => {
-  it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
-  });
+test('full app rendering/navigating', () => {
+  render(<App />, { wrapper: MemoryRouter });
+
+  // verify access to route links
+  expect(screen.getByRole('link', {name: /admin/i})).toBeInTheDocument();
+
+  expect(screen.getByRole('link', {name: /form configuration/i})).toBeInTheDocument();
+
+  expect(screen.getByRole('link', {name: /field forms/i})).toBeInTheDocument();
+
+  expect(screen.getByRole('link', {name: /data entry/i})).toBeInTheDocument();
+
+  expect(screen.getByRole('link', {name: /data reports/i})).toBeInTheDocument();
+
 });
