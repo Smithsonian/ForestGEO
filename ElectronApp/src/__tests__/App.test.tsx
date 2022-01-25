e22666884bac822665c80115b63e6d97bfc6c1f7
esscott <esscott@smith.edu>
2022-01-25 12:30:33 -0500

    Seperate test for full app rendering

--- ElectronApp/src/__tests__/App.test.tsx
+++ ElectronApp/src/__tests__/App.test.tsx
@@ -1,20 +1,10 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from '../renderer/App';
 
-test('full app rendering/navigating', () => {
-  render(<App />, { wrapper: MemoryRouter });
-
-  // verify access to route links
-  expect(screen.getByRole('link', {name: /admin/i})).toBeInTheDocument();
-
-  expect(screen.getByRole('link', {name: /form configuration/i})).toBeInTheDocument();
-
-  expect(screen.getByRole('link', {name: /field forms/i})).toBeInTheDocument();
-
-  expect(screen.getByRole('link', {name: /data entry/i})).toBeInTheDocument();
-
-  expect(screen.getByRole('link', {name: /data reports/i})).toBeInTheDocument();
-
+describe('App', () => {
+  it('should render', () => {
+    expect(render(<App />, { wrapper: MemoryRouter })).toBeTruthy();
+  });
 });
