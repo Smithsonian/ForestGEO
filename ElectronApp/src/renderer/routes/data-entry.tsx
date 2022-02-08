591ef153a196b83a39a2a1faf51031f03731e13b
esscott <esscott@smith.edu>
2022-02-08 10:56:32 -0500

    Create table component with mock data

--- ElectronApp/src/renderer/routes/data-entry.tsx
+++ ElectronApp/src/renderer/routes/data-entry.tsx
@@ -1,7 +1,10 @@
+import Table from '../components/Table';
+
 export default function DataEntry() {
   return (
     <div data-testid="data-entry">
       <h2>Data Entry</h2>
+      <Table />
     </div>
   );
 }
