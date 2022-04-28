1a97a470d51106bdc31d99672130a0862ec92fc6
René Dudfield <renesd@gmail.com>
2022-04-28 18:54:02 +0200

    frontend: Try out react-dropzone

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Button from '@mui/material/Button';
+import { useDropzone } from 'react-dropzone';
+
+function OurFriendlyDropzone() {
+  // @ts-ignore
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+    console.log('acceptedFiles', acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
+}
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  return (
+    <>
+      <OurFriendlyDropzone />
+      <Button variant="contained">Hello World</Button>
+    </>
+  );
 }
 
 export default App;
