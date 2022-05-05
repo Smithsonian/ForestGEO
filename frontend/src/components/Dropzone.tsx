e042f4181e9a4f5fe88a43b33b84b0adb8209034
esscott <esscott@smith.edu>
2022-05-05 15:26:47 -0400

    Use FileReader API to access file contents

--- frontend/src/components/Dropzone.tsx
+++ frontend/src/components/Dropzone.tsx
@@ -1,22 +1,37 @@
 import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import { Box } from '@mui/system';
 
 export default function Dropzone() {
   // @ts-ignore
   const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file: Blob) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log('file reading was aborted');
+      reader.onerror = () => console.log('file reading has failed');
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+      };
+      reader.readAsArrayBuffer(file);
+    });
     // Do something with the files
-    console.log('acceptedFiles', acceptedFiles);
+    // console.log('acceptedFiles', acceptedFiles);
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
-    </div>
+    <Box component="div" sx={{ p: 2, border: '1px dashed grey' }}>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+    </Box>
   );
 }
