a0ead21a33453ce66b1a2fffbb951a5fb48654b5
esscott <esscott@smith.edu>
2022-05-06 13:10:14 -0400

    Convert CSV to JSON with FileReader and Papa Parse

--- frontend/src/components/Dropzone.tsx
+++ frontend/src/components/Dropzone.tsx
@@ -1,21 +1,44 @@
 import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { Box } from '@mui/system';
+import { parse } from 'papaparse';
 
 export default function Dropzone() {
   // @ts-ignore
   const onDrop = useCallback((acceptedFiles) => {
-    acceptedFiles.forEach((file: Blob) => {
+    acceptedFiles.forEach((file: File) => {
+      if (file.type !== 'text/csv') {
+        // Not the right type of file, so we skip it for now.
+        alert(
+          'Only .csv files are supported. Uploaded file is called:' +
+            file.name +
+            ':'
+        );
+        // Skip this file
+        return;
+      }
+
       const reader = new FileReader();
 
-      reader.onabort = () => console.log('file reading was aborted');
-      reader.onerror = () => console.log('file reading has failed');
+      reader.onabort = () => alert('file reading was aborted');
+      reader.onerror = () => alert('file reading has failed');
       reader.onload = () => {
         // Do whatever you want with the file contents
-        const binaryStr = reader.result;
+        const binaryStr = reader.result as string;
         console.log(binaryStr);
+        console.log(parse(binaryStr));
+        const results = parse(binaryStr);
+
+        console.log(JSON.stringify(results.data));
+
+        if (results.errors.length) {
+          alert(
+            `Error on row: ${results.errors[0].row}. ${results.errors[0].message}`
+          );
+          // Only print the first error for now to avoid dialog clog
+        }
       };
-      reader.readAsArrayBuffer(file);
+      reader.readAsText(file);
     });
     // Do something with the files
     // console.log('acceptedFiles', acceptedFiles);
