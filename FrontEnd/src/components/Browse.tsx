15bf61093cc7d27bb3922e45a91fba68cfe48069
Jaro05 <62530116+Jaro05@users.noreply.github.com>
2022-07-16 13:45:51 -0400

    Added basic table to browse page

--- FrontEnd/src/components/Browse.tsx
+++ FrontEnd/src/components/Browse.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+} from '@mui/material';
 
 const Browse = () => {
   return (
-    <div>
-      <h2>Browse Page!</h2>
-    </div>
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Form</TableCell>
+            <TableCell>Quadrant</TableCell>
+            <TableCell>Date Entered</TableCell>
+            <TableCell>Validation</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>1</TableCell>
+            <TableCell>252</TableCell>
+            <TableCell>11/12/22</TableCell>
+            <TableCell>Jacob Rosen</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 export default Browse;
