734d0e1a43b9e19805e5ab891d9ef7469fa42d20
Alex Bondar <zmagar@gmail.com>
2022-08-29 11:00:04 -0700

    Select Plot component

--- FrontEnd/src/index.tsx
+++ FrontEnd/src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,6 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>
