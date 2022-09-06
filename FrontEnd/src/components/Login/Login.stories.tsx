7cd15db0c81c90e29632979a95d427009981db3f
René Dudfield <renesd@gmail.com>
2022-09-06 23:05:23 +0200

    Login: Move everything into a Login folder, refactor
    - Add a Login.stories.tsx
    - Separate presentation and logic
    - Move the login-image into the Login folder
    - WIP: move from html tags to mui components

---
+++ FrontEnd/src/components/Login/Login.stories.tsx
@@ -0,0 +1,21 @@
+import { MemoryRouter } from 'react-router-dom';
+
+import { ComponentMeta, Story } from '@storybook/react';
+import { LoginPure, LoginPureProps } from './Login';
+
+export default {
+  title: 'Login',
+  component: LoginPure,
+  argTypes: {},
+} as ComponentMeta<typeof LoginPure>;
+
+const Template: Story<LoginPureProps> = (args) => {
+  return (
+    <MemoryRouter initialEntries={['/login']}>
+      <LoginPure {...args} />
+    </MemoryRouter>
+  );
+};
+
+export const Login = Template.bind({});
+Login.args = {};
