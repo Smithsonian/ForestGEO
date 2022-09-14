bf67bf83c489bb81f25bcbffa9ba197620af22b5
René Dudfield <renesd@gmail.com>
2022-09-14 18:49:48 +0200

    FileList: Folderize, use mui components, add storybook
    We don't have a dependency on FileWithPath from react-dropzone.
    Instead we create an interface with just the fields we care about.

---
+++ FrontEnd/src/components/FileList/FileList.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentMeta, Story } from '@storybook/react';
+import FileList, { FileListProps } from './FileList';
+
+export default {
+  title: 'FileList',
+  component: FileList,
+  argTypes: {},
+} as ComponentMeta<typeof FileList>;
+
+const Template: Story<FileListProps> = (args) => <FileList {...args} />;
+
+export const SomeFiles = Template.bind({});
+SomeFiles.args = {
+  acceptedFilesList: [
+    { path: 'meow.csv', size: 22 },
+    { path: 'what-does-the-fox-say.csv', size: 243344444442 },
+  ],
+};
+
+export const NoFiles = Template.bind({});
+NoFiles.args = {
+  acceptedFilesList: [],
+};
---
+++ FrontEnd/src/components/FileList/index.tsx
@@ -0,0 +1,2 @@
+export { default } from './FileList';
+export * from './FileList';
