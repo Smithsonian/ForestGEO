b9b86fed45cad5057a1289c8e398e171e1e22d26
siddheshraze <siddheshraze@gmail.com>
2023-12-08 11:27:20 -0500

    removing extraneous comments, keeping broken code commented

--- NextJSApp/frontend/components/validationtable.tsx
+++ NextJSApp/frontend/components/validationtable.tsx
@@ -25,81 +25,81 @@ export interface DataStructure {
   [key: string]: string;
 }
 
-// export function DisplayErrorTable({fileName, fileData, errorMessage}: DisplayErrorTableProps) {
-//   return (
-//     <>
-//       <TableContainer component={Paper}>
-//         <h3>file: {fileName}</h3>
-//
-//         <Table>
-//           {errorMessage[fileName]['headers'] ? (
-//             <></>
-//           ) : (
-//             <>
-//               <TableHead>
-//                 <TableRow>
-//                   {tableHeaders.map((row, index) => {
-//                     return <TableCell key={index}>{row.label}</TableCell>;
-//                   })}
-//                 </TableRow>
-//               </TableHead>
-//               <TableBody>
-//                 {fileData!.data.map((data: DataStructure, rowIdx) => {
-//                   return (
-//                     <>
-//                       <TableRow>
-//                         {tableHeaders.map((header, i) => {
-//                           if (errorMessage[fileName][rowIdx]) {
-//                             let errInfo = errorMessage[fileName][rowIdx].split('::');
-//                             console.log(errInfo);
-//                             if (errInfo[1] == header.label && errInfo[0] == 'MValue') {
-//                               return (
-//                                 <>
-//                                   <TableCell key={i} sx={{color: 'red', fontWeight: 'bold'}}>
-//                                     Missing Value!
-//                                   </TableCell>
-//                                 </>
-//                               );
-//                             } else if (errInfo[1] == header.label && errInfo[0] == 'WFormat') {
-//                               return (
-//                                 <>
-//                                   <TableCell key={i} sx={{color: 'red', fontWeight: 'bold'}}>
-//                                     {data[header.label]} <br/>
-//                                     Wrong Format!
-//                                   </TableCell>
-//                                 </>
-//                               );
-//                             } else {
-//                               return (
-//                                 <>
-//                                   <TableCell key={i} sx={{color: 'red'}}>
-//                                     {data[header.label]}
-//                                   </TableCell>
-//                                 </>
-//                               );
-//                             }
-//                           } else {
-//                             return (
-//                               <>
-//                                 <TableCell key={i}>
-//                                   {data[header.label]}
-//                                 </TableCell>
-//                               </>
-//                             );
-//                           }
-//                         })}
-//                       </TableRow>
-//                     </>
-//                   );
-//                 })}
-//               </TableBody>
-//             </>
-//           )}
-//         </Table>
-//       </TableContainer>
-//     </>
-//   );
-// }
+export function DisplayErrorTable({fileName, fileData, errorMessage}: DisplayErrorTableProps) {
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <h3>file: {fileName}</h3>
+
+        <Table>
+          {errorMessage[fileName]['headers'] ? (
+            <></>
+          ) : (
+            <>
+              <TableHead>
+                <TableRow>
+                  {/*{tableHeaders.map((row, index) => {*/}
+                  {/*  return <TableCell key={index}>{row.label}</TableCell>;*/}
+                  {/*})}*/}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {fileData!.data.map((data: DataStructure, rowIdx) => {
+                  return (
+                    <>
+                      <TableRow>
+                        {/*{tableHeaders.map((header, i) => {*/}
+                        {/*  if (errorMessage[fileName][rowIdx]) {*/}
+                        {/*    let errInfo = errorMessage[fileName][rowIdx].split('::');*/}
+                        {/*    console.log(errInfo);*/}
+                        {/*    if (errInfo[1] == header.label && errInfo[0] == 'MValue') {*/}
+                        {/*      return (*/}
+                        {/*        <>*/}
+                        {/*          <TableCell key={i} sx={{color: 'red', fontWeight: 'bold'}}>*/}
+                        {/*            Missing Value!*/}
+                        {/*          </TableCell>*/}
+                        {/*        </>*/}
+                        {/*      );*/}
+                        {/*    } else if (errInfo[1] == header.label && errInfo[0] == 'WFormat') {*/}
+                        {/*      return (*/}
+                        {/*        <>*/}
+                        {/*          <TableCell key={i} sx={{color: 'red', fontWeight: 'bold'}}>*/}
+                        {/*            {data[header.label]} <br/>*/}
+                        {/*            Wrong Format!*/}
+                        {/*          </TableCell>*/}
+                        {/*        </>*/}
+                        {/*      );*/}
+                        {/*    } else {*/}
+                        {/*      return (*/}
+                        {/*        <>*/}
+                        {/*          <TableCell key={i} sx={{color: 'red'}}>*/}
+                        {/*            {data[header.label]}*/}
+                        {/*          </TableCell>*/}
+                        {/*        </>*/}
+                        {/*      );*/}
+                        {/*    }*/}
+                        {/*  } else {*/}
+                        {/*    return (*/}
+                        {/*      <>*/}
+                        {/*        <TableCell key={i}>*/}
+                        {/*          {data[header.label]}*/}
+                        {/*        </TableCell>*/}
+                        {/*      </>*/}
+                        {/*    );*/}
+                        {/*  }*/}
+                        {/*})}*/}
+                      </TableRow>
+                    </>
+                  );
+                })}
+              </TableBody>
+            </>
+          )}
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
 
 /**
  * Shows a data table with the possibility of showing errors.
@@ -216,34 +216,34 @@ export function ValidationErrorTable({uploadedData, errorMessage, headers,}: Val
 }
 
 export function DisplayParsedData(fileData: { fileName: string; data: DataStructure[] }) {
-  // return (
-  //   <>
-  //     <TableContainer component={Paper} key={fileData.fileName}>
-  //       <Table>
-  //         <TableHead>
-  //           <TableRow>
-  //             {tableHeaders.map((row, index) => {
-  //               return <TableCell key={index}>{row.label}</TableCell>;
-  //             })}
-  //           </TableRow>
-  //         </TableHead>
-  //         <TableBody>
-  //           {fileData!.data.map((data: DataStructure) => {
-  //             return (
-  //               <>
-  //                 <TableRow>
-  //                   {tableHeaders.map((header, i) => (
-  //                     <TableCell key={i}>
-  //                       {data[header.label]}
-  //                     </TableCell>
-  //                   ))}
-  //                 </TableRow>
-  //               </>
-  //             );
-  //           })}
-  //         </TableBody>
-  //       </Table>
-  //     </TableContainer>
-  //   </>
-  // );
+  return (
+    <>
+      <TableContainer component={Paper} key={fileData.fileName}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {/*{tableHeaders.map((row, index) => {*/}
+              {/*  return <TableCell key={index}>{row.label}</TableCell>;*/}
+              {/*})}*/}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {fileData!.data.map((data: DataStructure) => {
+              return (
+                <>
+                  <TableRow>
+                    {/*{tableHeaders.map((header, i) => (*/}
+                    {/*  <TableCell key={i}>*/}
+                    {/*    {data[header.label]}*/}
+                    {/*  </TableCell>*/}
+                    {/*))}*/}
+                  </TableRow>
+                </>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
