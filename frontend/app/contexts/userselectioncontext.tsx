244979f6293823b11362518c65a282f596611274
siddheshraze <81591724+siddheshraze@users.noreply.github.com>
2024-01-11 12:54:56 -0500

    created new Census object that will sit alongside the Plot object for user selection when uploading files

--- frontend/app/contexts/userselectioncontext.tsx
+++ frontend/app/contexts/userselectioncontext.tsx
@@ -1,25 +1,35 @@
 "use client";
 import React, {createContext, Dispatch, useContext, useReducer} from "react";
-import {allCensus, allQuadrats, CensusAction, Plot, PlotAction, plots, QuadratsAction} from "@/config/macros";
+import {
+  allCensus,
+  allQuadrats,
+  Census,
+  CensusAction,
+  containsPlotCensusNumber,
+  Plot,
+  PlotAction,
+  plots,
+  QuadratsAction
+} from "@/config/macros";
 import {useCensusListContext, usePlotListContext, useQuadratListContext} from "@/app/contexts/generalcontext";
 
 export const PlotsContext = createContext<Plot | null>(null);
-export const CensusContext = createContext<number | null>(null);
+export const CensusContext = createContext<Census | null>(null);
 export const QuadratContext = createContext<number | null>(null);
 export const PlotsDispatchContext = createContext<Dispatch<{ plotKey: string | null }> | null>(null);
-export const CensusDispatchContext = createContext<Dispatch<{ census: number | null }> | null>(null);
+export const CensusDispatchContext = createContext<Dispatch<{ census: Census | null }> | null>(null);
 export const QuadratDispatchContext = createContext<Dispatch<{ quadrat: number | null }> | null>(null);
 
 export default function PlotProvider({children}: { children: React.ReactNode }) {
   const plotListContext = (usePlotListContext() ?? JSON.parse(localStorage.getItem('plotList') ?? '[]')) || plots;
-  const censusListContext = useCensusListContext() ?? JSON.parse(localStorage.getItem('censusList') ?? JSON.stringify(allCensus));
+  const censusListContext = useCensusListContext() ?? JSON.parse(localStorage.getItem('censusList') ?? '[]');
   const quadratListContext = useQuadratListContext() ?? JSON.parse(localStorage.getItem('quadratList') ?? JSON.stringify(allQuadrats));
   const [plot, plotDispatch] = useReducer(
     (state: Plot | null, action: PlotAction) => plotsReducer(state, action, plotListContext),
     null
   );
   const [census, censusDispatch] = useReducer(
-    (state: number | null, action: CensusAction) => censusReducer(state, action, censusListContext),
+    (state: Census | null, action: CensusAction) => censusReducer(state, action, censusListContext),
     null
   );
 
@@ -49,13 +59,13 @@ function plotsReducer(currentPlot: any, action: { plotKey: string | null }, plot
   if (action.plotKey == null) return null;
   else {
     const foundPlot = plotListContext.find((p) => p.key === action.plotKey);
-    return foundPlot || currentPlot;
+    return foundPlot ?? currentPlot;
   }
 }
 
-function censusReducer(currentCensus: number | null, action: { census: number | null }, censusListContext: number[]) {
+function censusReducer(currentCensus: Census | null, action: { census: Census | null }, censusListContext: Census[]) {
   if (action.census == null) return null;
-  else if (censusListContext.includes(action.census)) return action.census;
+  else if (containsPlotCensusNumber(censusListContext, action.census.plotCensusNumber)) return action.census;
   else return currentCensus;
 }
 
