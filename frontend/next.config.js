d42255aa4b0c408e475937417b63f07e97678c7b
siddheshraze <siddheshraze@gmail.com>
2024-02-27 13:39:43 -0500

    removing next build cache statement

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,10 +6,6 @@ const nextConfig = {
     },
   },
   output: 'standalone',
-  generateBuildId: async () => {
-    // This could be anything, using the latest git hash
-    return process.env.GIT_HASH
-  },
 }
 
 module.exports = nextConfig
