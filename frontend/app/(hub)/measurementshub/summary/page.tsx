886e966599a58f47dc5d3cdb22f9f99515511ef0
sambokar <siddheshraze@gmail.com>
2024-05-19 11:21:02 -0400

    removing progress dialog's independent API calls and replacing with connection to existing Context data validity

--- frontend/app/(hub)/measurementshub/summary/page.tsx
+++ frontend/app/(hub)/measurementshub/summary/page.tsx
@@ -1,9 +1,9 @@
 'use client';
-import React, {useEffect, useState} from "react";
-import {GridRowModes, GridRowModesModel, GridRowsProp} from "@mui/x-data-grid";
-import {Alert, AlertProps, LinearProgress, Tooltip, TooltipProps, styled, tooltipClasses} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { GridRowModes, GridRowModesModel, GridRowsProp } from "@mui/x-data-grid";
+import { Alert, AlertProps, LinearProgress, Tooltip, TooltipProps, styled, tooltipClasses } from "@mui/material";
 import DataGridCommons from "@/components/datagrids/datagridcommons";
-import {MeasurementsSummaryGridColumns} from '@/config/sqlrdsdefinitions/views/measurementssummaryviewrds';
+import { MeasurementsSummaryGridColumns } from '@/config/sqlrdsdefinitions/views/measurementssummaryviewrds';
 import {
   Box,
   IconButton,
@@ -20,8 +20,8 @@ import {
   Snackbar,
   Stack,
 } from "@mui/joy";
-import Select, {SelectOption} from "@mui/joy/Select";
-import {useSession} from "next-auth/react";
+import Select, { SelectOption } from "@mui/joy/Select";
+import { useSession } from "next-auth/react";
 import {
   useCensusContext,
   usePlotContext,
@@ -29,23 +29,23 @@ import {
   useQuadratDispatch,
   useSiteContext
 } from "@/app/contexts/userselectionprovider";
-import {randomId} from "@mui/x-data-grid-generator";
+import { randomId } from "@mui/x-data-grid-generator";
 import UploadParentModal from "@/components/uploadsystemhelpers/uploadparentmodal";
-import {useQuadratListContext} from "@/app/contexts/listselectionprovider";
-import {Quadrat} from "@/config/sqlrdsdefinitions/tables/quadratrds";
+import { useQuadratListContext } from "@/app/contexts/listselectionprovider";
+import { Quadrat } from "@/config/sqlrdsdefinitions/tables/quadratrds";
 import Option from '@mui/joy/Option';
 import MeasurementSummaryGrid from "@/components/datagrids/msvdatagrid";
 import { useDataValidityContext } from "@/app/contexts/datavalidityprovider";
+import { UnifiedValidityFlags } from "@/config/macros";
 
-const LargeTooltip = styled(({className, ...props}: TooltipProps) => (
-  <Tooltip {...props} classes={{popper: className}}/>
-))(({theme}) => ({
+const LargeTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
   [`& .${tooltipClasses.tooltip}`]: {
     fontSize: 16,
-    maxWidth: 600,  // Increase maxWidth to give more space for text
+    maxWidth: 600, // Increase maxWidth to give more space for text
   },
 }));
-
 
 interface ChecklistProgress {
   progress: number;
@@ -53,13 +53,9 @@ interface ChecklistProgress {
   error?: string;
 }
 
-interface ChecklistProgressMap {
-  [key: string]: ChecklistProgress;
-}
-
 export default function SummaryPage() {
-  const {data: session} = useSession();
-  const [quadrat, setQuadrat] = useState<Quadrat>(null);
+  const { data: session } = useSession();
+  const [quadrat, setQuadrat] = useState<Quadrat | null>(null);
   const [quadratList, setQuadratList] = useState<Quadrat[] | null>([]);
   let currentPlot = usePlotContext();
   let currentCensus = useCensusContext();
@@ -67,13 +63,19 @@ export default function SummaryPage() {
   let quadratListContext = useQuadratListContext();
   let currentQuadrat = useQuadratContext();
   const quadratDispatch = useQuadratDispatch();
+  const { validity, recheckValidityIfNeeded } = useDataValidityContext();
+  const [progressDialogOpen, setProgressDialogOpen] = useState(false);
+  const [isUploadAllowed, setIsUploadAllowed] = useState(false);
+  const [isUploadModalOpen, setIsUploadModalOpen] = useState(false);
+  const [triggerGlobalError, setTriggerGlobalError] = useState(false);
+  const [globalError, setGlobalError] = useState<string | null>(null);
 
   useEffect(() => {
     if (currentPlot !== null) {
       // ensure that selectable list is restricted by selected plot
       setQuadratList(quadratListContext?.filter(quadrat => quadrat?.plotID === currentPlot.id) || null);
     }
-  }, []);
+  }, [currentPlot, quadratListContext]);
 
   const initialRows: GridRowsProp = [
     {
@@ -108,13 +110,11 @@ export default function SummaryPage() {
       attributes: [],
     }
   ];
+
   const [rows, setRows] = React.useState(initialRows);
-  const [rowCount, setRowCount] = useState(0);  // total number of rows
+  const [rowCount, setRowCount] = useState(0); // total number of rows
   const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>({});
-  const [snackbar, setSnackbar] = React.useState<Pick<
-    AlertProps,
-    'children' | 'severity'
-  > | null>(null);
+  const [snackbar, setSnackbar] = React.useState<Pick<AlertProps, 'children' | 'severity'> | null>(null);
   const [refresh, setRefresh] = useState(false);
   const [paginationModel, setPaginationModel] = useState({
     page: 0,
@@ -122,78 +122,16 @@ export default function SummaryPage() {
   });
   const [isNewRowAdded, setIsNewRowAdded] = useState<boolean>(false);
   const [shouldAddRowAfterFetch, setShouldAddRowAfterFetch] = useState(false);
-  const [isUploadAllowed, setIsUploadAllowed] = useState(false);
-  const [progressDialogOpen, setProgressDialogOpen] = useState(false);
-  const [checklistProgress, setChecklistProgress] = useState<ChecklistProgressMap>({});
-  const [isUploadModalOpen, setIsUploadModalOpen] = useState(false);
-  const [triggerGlobalError, setTriggerGlobalError] = useState(false);
-  const [globalError, setGlobalError] = useState<string | null>(null);
-  let {validity} = useDataValidityContext();
 
   useEffect(() => {
-      const verifyPreconditions = async () => {
-          const checklist = ['attributes', 'species', 'personnel', 'quadrats', 'subquadrats'];
-          let allValid = true;
+    const verifyPreconditions = async () => {
+      setIsUploadAllowed(!Object.values(validity).includes(false));
+    };
 
-          for (const item of checklist) {
-            const url = `/api/cmprevalidation/${item}/${currentSite?.schemaName}/${currentPlot?.id}/${currentCensus?.censusID}`;
-
-            try {
-              const response = await fetch(url, {method: 'GET'});
-              if (!response.ok) {
-                throw new Error(`Failed: ${response.statusText}`);
-              }
-              if (item === 'quadrats')
-                setChecklistProgress(prevState => ({
-                  ...prevState,
-                  [item]: {
-                    progress: 100,
-                    message: "Passed: " + item.charAt(0).toUpperCase() + item.substring(1),
-                    error: undefined
-                  }
-                }));
-            } catch (err: any) {
-              if (item === 'quadrats') {
-                setChecklistProgress(prevState => ({
-                  ...prevState,
-                  [item]: {
-                    progress: 100, // Ensure progress is 100% even on failure
-                    message: "Failure: " + item.charAt(0).toUpperCase() + item.substring(1),
-                    error: "A plot must contain at least 1 quadrat for the current census!"
-                  }
-                }));
-              } else if (item === 'subquadrats') {
-                setChecklistProgress(prevState => ({
-                  ...prevState,
-                  [item]: {
-                    progress: 100, // Ensure progress is 100% even on failure
-                    message: "Failure: " + item.charAt(0).toUpperCase() + item.substring(1),
-                    error: "Each quadrat in the current census and plot must contain at least 1 subquadrat!"
-                  }
-                }));
-              } else {
-                setChecklistProgress(prevState => ({
-                  ...prevState,
-                  [item]: {
-                    progress: 100, // Ensure progress is 100% even on failure
-                    message: "Failure: " + item.charAt(0).toUpperCase() + item.substring(1),
-                    error: "Table must contain at least 1 row of data!"
-                  }
-                }));
-              }
-              allValid = false;
-            }
-          }
-          setIsUploadAllowed(allValid);
-        }
-      ;
-
-      if (progressDialogOpen) {
-        verifyPreconditions().catch(console.error);
-      }
-    }, [progressDialogOpen]
-  )
-  ;
+    if (progressDialogOpen) {
+      verifyPreconditions().catch(console.error);
+    }
+  }, [progressDialogOpen, validity]);
 
   const addNewRowToGrid = () => {
     const id = randomId();
@@ -231,7 +169,7 @@ export default function SummaryPage() {
       isNew: true,
     };
     setRows(oldRows => [...oldRows, newRow]);
-    setRowModesModel(oldModel => ({...oldModel, [id]: {mode: GridRowModes.Edit}}));
+    setRowModesModel(oldModel => ({ ...oldModel, [id]: { mode: GridRowModes.Edit } }));
   };
 
   const handleCloseGlobalError = () => {
@@ -245,18 +183,23 @@ export default function SummaryPage() {
       setTimeout(() => {
         setIsUploadModalOpen(true);
       }, 300);
+    } else {
+      setGlobalError('Missing prerequisites! Please upload supporting data before submitting measurements!');
+      setTriggerGlobalError(true);
     }
   };
+
+  const checklistItems: (keyof UnifiedValidityFlags)[] = ['attributes', 'species', 'personnel', 'quadrats', 'subquadrats'];
+
   const ProgressDialog = () => (
     <Modal
       open={progressDialogOpen}
-      onClose={() => {
-      }}
-      sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}
+      onClose={() => {}}
+      sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
     >
       <ModalDialog
         size="lg"
-        sx={{width: '100%', maxHeight: '100vh', overflow: 'auto'}}
+        sx={{ width: '100%', maxHeight: '100vh', overflow: 'auto' }}
         role="alertdialog"
       >
         <DialogTitle>Pre-Validation Systems Check</DialogTitle>
@@ -266,33 +209,27 @@ export default function SummaryPage() {
             In order to upload measurements, all of the following tables must be populated!
           </Typography>
           <List>
-            {['attributes', 'species', 'personnel', 'quadrats', 'subquadrats'].map((item, index) => {
-              const progressData = checklistProgress[item] || {progress: 0, message: "Pending..."};
+            {checklistItems.map((item) => {
+              const isValid = validity[item];
+              const progressData = isValid
+                ? { progress: 100, message: `Passed: ${item.charAt(0).toUpperCase() + item.substring(1)}`, error: undefined }
+                : { progress: 0, message: `Failure: ${item.charAt(0).toUpperCase() + item.substring(1)}`, error: `${item.charAt(0).toUpperCase() + item.substring(1)} is invalid or missing.` };
               const tooltipMessage = progressData.error
                 ? `${progressData.error}`
                 : progressData.message;
 
               return (
-                <ListItem sx={{alignItems: 'center', display: 'flex', flexDirection: 'row'}} key={item}>
-                  <ListItemContent
-                    sx={{minWidth: '160px', mr: 2, my: 'auto'}}> {/* Ensure vertical centering with margin auto */}
+                <ListItem sx={{ alignItems: 'center', display: 'flex', flexDirection: 'row' }} key={item}>
+                  <ListItemContent sx={{ minWidth: '160px', mr: 2, my: 'auto' }}>
                     <Typography level={'body-md'}>{progressData.message}</Typography>
                   </ListItemContent>
                   <LargeTooltip title={tooltipMessage} placement="top" arrow>
-                    <Box sx={{
-                      width: '100%',
-                      maxWidth: 'calc(100% - 180px)',
-                      my: 'auto'
-                    }}> {/* Adjust maxWidth accordingly */}
+                    <Box sx={{ width: '100%', maxWidth: 'calc(100% - 180px)', my: 'auto' }}>
                       <LinearProgress
                         variant="determinate"
                         value={progressData.progress}
-                        color={progressData.error ? 'error' : 'primary'}
-                        sx={{
-                          width: '100%',
-                          height: 8, // Ensure the progress bar is visible enough
-                          // backgroundColor: progressData.error ? 'error.main' : 'primary.main', // Use backgroundColor if bgcolor isn't effective
-                        }}
+                        color={isValid ? 'primary' : 'error'}
+                        sx={{ width: '100%', height: 8 }}
                       />
                     </Box>
                   </LargeTooltip>
@@ -305,11 +242,6 @@ export default function SummaryPage() {
           <Button
             onClick={() => {
               setProgressDialogOpen(false);
-              setChecklistProgress({});
-              if (!isUploadAllowed) {
-                setGlobalError('Missing prerequisites! Please upload supporting data before submitting measurements!');
-                setTriggerGlobalError(true);
-              }
             }}
             color="primary"
           >
@@ -332,19 +264,18 @@ export default function SummaryPage() {
       return <Typography>Select a Quadrat</Typography>; // or some placeholder JSX
     }
 
-    // Find the corresponding CensusRDS object
+    // Find the corresponding Quadrat object
     const selectedValue = option.value; // assuming option has a 'value' property
     const selectedQuadrat = quadratListContext?.find(c => c?.quadratName === selectedValue);
 
     // Return JSX
-    return selectedQuadrat ? <Typography>{`Quadrat: ${selectedQuadrat?.quadratName}`}</Typography> :
-      <Typography>No Quadrat</Typography>;
+    return selectedQuadrat ? <Typography>{`Quadrat: ${selectedQuadrat?.quadratName}`}</Typography> : <Typography>No Quadrat</Typography>;
   };
 
   const handleQuadratSelection = async (selectedQuadrat: Quadrat | null) => {
     setQuadrat(selectedQuadrat);
     if (quadratDispatch) {
-      await quadratDispatch({quadrat: selectedQuadrat});
+      await quadratDispatch({ quadrat: selectedQuadrat });
     }
   };
 
@@ -371,7 +302,7 @@ export default function SummaryPage() {
         <Option value="">None</Option>
         {quadratList?.map((item) => (
           <Option value={item?.quadratName} key={item?.quadratName}>
-            <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
               <Typography level="body-lg">{item?.quadratName}</Typography>
             </Box>
           </Option>
@@ -379,8 +310,8 @@ export default function SummaryPage() {
       </Select>
       <Button onClick={handleConfirmQuadrat} size="sm" color="primary">Confirm</Button>
       {!validity['quadrats'] && (
-        <Alert severity="warning" sx={{mt: 2}}>
-          <Typography level="body-lg" color="warning">No quadrats exist to be </Typography>
+        <Alert severity="warning" sx={{ mt: 2 }}>
+          <Typography level="body-lg" color="warning">No quadrats exist to be selected.</Typography>
         </Alert>
       )}
     </Stack>
@@ -393,37 +324,39 @@ export default function SummaryPage() {
           <Alert onClose={handleCloseGlobalError} severity="error">{globalError}</Alert>
         </Snackbar>
       )}
-      <Box sx={{display: 'flex', alignItems: 'center', mb: 3, width: '100%'}}>
-        <ProgressDialog/>
+      <Box sx={{ display: 'flex', alignItems: 'center', mb: 3, width: '100%' }}>
+        <ProgressDialog />
         <Box sx={{
           width: '100%', display: 'flex', justifyContent: 'space-between',
           alignItems: 'center', backgroundColor: 'warning.main', borderRadius: '4px', p: 2
         }}>
           <Stack direction="column">
-            <Box sx={{display: 'flex', flexDirection: 'column', flexGrow: 1}}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
               {session?.user.isAdmin && (
-                <Typography level={"title-lg"} sx={{color: "#ffa726"}}>Note: ADMINISTRATOR VIEW</Typography>
+                <Typography level={"title-lg"} sx={{ color: "#ffa726" }}>Note: ADMINISTRATOR VIEW</Typography>
               )}
-              <Typography level={"title-md"} sx={{color: "#ffa726"}}>Note: This is a locked view and will not allow
+              <Typography level={"title-md"} sx={{ color: "#ffa726" }}>Note: This is a locked view and will not allow
                 modification.</Typography>
-              <Typography level={"body-md"} sx={{color: "#ffa726"}}>Please use this view as a way to confirm changes
-                made
-                to measurements.</Typography>
+              <Typography level={"body-md"} sx={{ color: "#ffa726" }}>Please use this view as a way to confirm changes
+                made to measurements.</Typography>
             </Box>
           </Stack>
-          <Box sx={{flexGrow: 1, display: 'flex', justifyContent: 'left', marginLeft: '5%'}}>
-            <QuadratSelectionMenu/>
+          <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'left', marginLeft: '5%' }}>
+            <QuadratSelectionMenu />
           </Box>
-          <Box sx={{display: 'flex', alignItems: 'center', gap: 2}}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
             <Button onClick={() => setProgressDialogOpen(true)} variant="solid" color="primary">Upload</Button>
           </Box>
         </Box>
       </Box>
-      <UploadParentModal isUploadModalOpen={isUploadModalOpen}
-                         handleCloseUploadModal={() => {
-                           setIsUploadModalOpen(false);
-                           setRefresh(true);
-                         }} formType={"measurements"}/>
+      <UploadParentModal
+        isUploadModalOpen={isUploadModalOpen}
+        handleCloseUploadModal={() => {
+          setIsUploadModalOpen(false);
+          setRefresh(true);
+        }}
+        formType={"measurements"}
+      />
       <MeasurementSummaryGrid
         locked={!validity['quadrats'] && !currentQuadrat}
         gridColumns={MeasurementsSummaryGridColumns}
