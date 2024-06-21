cd477bf63327310774439869f7d971f3da993262
sambokar <siddheshraze@gmail.com>
2024-06-21 12:04:34 -0400

    not sure why, but next-auth login is not being permitted on development site for some reason

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -7,5 +7,12 @@ module.exports = {
       watch: false,
       autorestart: true,
     },
+    {
+      name: "forestgeo-livesite-development",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start -p " + (process.env.PORT || 3000),
+      watch: false,
+      autorestart: true,
+    },
   ],
 };
