75bb4c64f23b1d94cdeb3e5ef784e91702b8b9e9
sambokar <siddheshraze@gmail.com>
2024-09-26 07:51:52 -0400

    simple API endpoint to take signed in user's information and retrieve unique catalog UserID value for use. Pausing development of staging system to see if I can leverage existing UserDefinedFields column instead of needing to add a full new table

---
+++ frontend/app/api/catalog/[firstName]/[lastName]/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { PoolConnection } from 'mysql2/promise';
+import { getConn, runQuery } from '@/components/processors/processormacros';
+import { HTTPResponses } from '@/config/macros';
+
+export async function GET(_request: NextRequest, { params }: { params: { firstName: string; lastName: string } }) {
+  const { firstName, lastName } = params;
+  if (!firstName || !lastName) throw new Error('no first or last name provided!');
+
+  let conn: PoolConnection | null = null;
+
+  try {
+    conn = await getConn();
+    const query = `SELECT UserID FROM catalog.users WHERE FirstName = ? AND LastName = ?;`;
+    const results = await runQuery(conn, query, [firstName, lastName]);
+    if (results.length === 0) {
+      throw new Error('User not found');
+    }
+    return new NextResponse(JSON.stringify(results[0].UserID), { status: HTTPResponses.OK });
+  } catch (e: any) {
+    console.error('Error in GET request:', e.message);
+    return new NextResponse(JSON.stringify({ error: e.message }), { status: HTTPResponses.INTERNAL_SERVER_ERROR });
+  } finally {
+    if (conn) conn.release();
+  }
+}
