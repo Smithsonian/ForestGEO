00e115f9d1247010e748c2bd53b1d855702002d1
siddheshraze <81591724+siddheshraze@users.noreply.github.com>
2025-02-03 11:17:00 -0500

    octokit errors found. Rewinding and fixing

--- frontend/cypress/component/headerHeader.cy.tsx
+++ frontend/cypress/component/headerHeader.cy.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Header from '../../components/header'
+import React from 'react';
+import Header from '../../components/header';
 
 describe('<Header />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Header />)
-  })
-})
+    cy.mount(<Header />);
+  });
+});
