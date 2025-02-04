846a566e1b4cd9acd9aeae60ae6fd93e3f66cbf3
siddheshraze <81591724+siddheshraze@users.noreply.github.com>
2025-02-04 14:23:17 -0500

    continuing testing development. Adding/removing packages as needed

--- frontend/cypress/components/app/page/errorpage.cy.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ErrorPage from '@/app/error';
-
-describe('App --> Page --> <ErrorPage />', () => {
-  it('renders', () => {
-    cy.mount(<ErrorPage error={new Error('Test Error')} reset={function (): void {}} />);
-  });
-});
