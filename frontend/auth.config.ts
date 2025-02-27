9dcf07567f9a1f2d4d4b50686e72755fe367a713
sambokar <siddheshraze@gmail.com>
2025-02-27 16:38:11 -0500

    trying to manually set authorization scope in auth config. The system is returning a 403 forbidden access denied callback at the callback point. attempting to remove forced redirect to see if that makes a difference

--- frontend/auth.config.ts
+++ frontend/auth.config.ts
@@ -3,5 +3,13 @@ import MicrosoftEntraID from '@auth/core/providers/microsoft-entra-id';
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [MicrosoftEntraID]
+  providers: [
+    MicrosoftEntraID({
+      authorization: {
+        params: {
+          scope: 'openid profile email User.Read'
+        }
+      }
+    })
+  ]
 } satisfies NextAuthConfig;
