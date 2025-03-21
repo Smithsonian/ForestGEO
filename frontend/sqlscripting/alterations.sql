a673b7d202afdf049cdcb59df9694219dad4b888
sambokar <siddheshraze@gmail.com>
2025-03-21 10:36:21 -0400

    adding missing handling to pre-processing for incorrectly formatted codes, recording table alterations, and clarifying output messages

---
+++ frontend/sqlscripting/alterations.sql
@@ -0,0 +1,34 @@
+alter table coremeasurements modify MeasuredDBH decimal(12, 6) null;
+alter table coremeasurements modify MeasuredHOM decimal(12, 6) null;
+
+alter table failedmeasurements modify X decimal(12, 6) null;
+alter table failedmeasurements modify Y decimal(12, 6) null;
+alter table failedmeasurements modify DBH decimal(12, 6) null;
+alter table failedmeasurements modify HOM decimal(12, 6) null;
+
+alter table measurementssummary modify StemLocalX decimal(12, 6) null;
+alter table measurementssummary modify StemLocalY decimal(12, 6) null;
+alter table measurementssummary modify MeasuredDBH decimal(12, 6) null;
+alter table measurementssummary modify MeasuredHOM decimal(12, 6) null;
+
+alter table plots modify GlobalX decimal(12, 6) null;
+alter table plots modify GlobalY decimal(12, 6) null;
+alter table plots modify GlobalZ decimal(12, 6) null;
+alter table plots modify DimensionX decimal(12, 6) null;
+alter table plots modify DimensionY decimal(12, 6) null;
+alter table plots modify Area decimal(12, 6) null;
+
+alter table quadrats modify StartX decimal(12, 6) null;
+alter table quadrats modify StartY decimal(12, 6) null;
+alter table quadrats modify Area decimal(12, 6) null;
+
+alter table stems modify LocalX decimal(12, 6) null;
+alter table stems modify LocalY decimal(12, 6) null;
+
+alter table temporarymeasurements modify LocalX decimal(12, 6) null;
+alter table temporarymeasurements modify LocalY decimal(12, 6) null;
+alter table temporarymeasurements modify DBH decimal(12, 6) null;
+alter table temporarymeasurements modify HOM decimal(12, 6) null;
+
+alter table specieslimits modify LowerBound decimal(12, 6) null;
+alter table specieslimits modify UpperBound decimal(12, 6) null;
