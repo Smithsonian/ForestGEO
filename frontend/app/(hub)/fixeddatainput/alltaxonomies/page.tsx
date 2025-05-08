317c90c9ef412192178392b2b15a189f80683ecd
sambokar <siddheshraze@gmail.com>
2025-05-08 13:32:41 -0400

    updated grid explanations. fixed broken postvalidationqueries fetch query. added mapping structures between datagrids and forms. Additional documentation formatting and changes applied.

--- frontend/app/(hub)/fixeddatainput/alltaxonomies/page.tsx
+++ frontend/app/(hub)/fixeddatainput/alltaxonomies/page.tsx
@@ -1,11 +1,11 @@
-import RenderGridExplanations from '@/components/client/rendergridexplanations';
+import RenderGridFormExplanations from '@/components/client/rendergridformexplanations';
 import IsolatedAllTaxonomiesViewDataGrid from '@/components/datagrids/applications/isolated/isolatedalltaxonomiesdatagrid';
 import { DatagridType } from '@/config/macros/formdetails';
 
 export default function AllTaxonomiesPage() {
   return (
     <>
-      <RenderGridExplanations datagridType={DatagridType.alltaxonomiesview} />
+      <RenderGridFormExplanations datagridType={DatagridType.alltaxonomiesview} />
       <IsolatedAllTaxonomiesViewDataGrid />
     </>
   );
